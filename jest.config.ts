5d2ea0a75949e84d8ffcbd6062ee0670170926d9
ATS - Elhakim Cool <bagusajieiswara30@gmail.com>
2025-02-14 20:12:08 +0700

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/RqQy5k28fw

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'react-next-invoice ',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/react-next-invoice ',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
